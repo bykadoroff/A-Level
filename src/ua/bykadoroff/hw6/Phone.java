67617af7272dbfcf790a4f1874724d9561f30f2a
bykadoroff <bykadorov@gmail.com>
2021-09-26 19:00:27 +0300

    hw6 + gitignore

--- src/ua/bykadoroff/hw6/Phone.java
+++ src/ua/bykadoroff/hw6/Phone.java
@@ -1,4 +1,34 @@
 package ua.bykadoroff.hw6;
 
 public class Phone {
+
+
+    protected String number;
+    protected String model;
+    protected double weight;
+
+
+    protected void receiveCall(String name) {
+        System.out.println("Звонит: " + name);
+    }
+
+    protected String getNumber() {
+        return number;
+    }
+
+
+    Phone(String number, String model, double weight) {
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+    }
+
+    Phone(String number, String model) {
+        this.number = number;
+        this.model = model;
+    }
+
+    Phone(){}
+
+
 }
--- src/ua/bykadoroff/hw6/Task1.java
+++ src/ua/bykadoroff/hw6/Task1.java
@@ -1,4 +1,37 @@
 package ua.bykadoroff.hw6;
 
 public class Task1 {
+
+    public static void main(String[] args) {
+
+        Phone apple = new Phone();
+        Phone samsung = new Phone();
+        Phone huawei = new Phone();
+
+
+        apple.number = "+380954893333";
+        apple.model = "Iphone SE 2020";
+        apple.weight = 148.5;
+        System.out.println("Apple: " + apple.model + ", " + apple.weight + " grams" + ", number: " + apple.number);
+        apple.receiveCall("Gulia");
+        System.out.println(apple.getNumber());
+        
+
+        samsung.number = "+380957613850";
+        samsung.model = "A52";
+        samsung.weight = 152.3;
+        System.out.println("Samsung: " + samsung.model + ", " + samsung.weight + " grams" + ", number: " + samsung.number);
+        samsung.receiveCall("Serg");
+        System.out.println(apple.getNumber());
+
+
+        huawei.number = "+380506049986";
+        huawei.model = "P40 Lite";
+        huawei.weight = 183.7;
+        System.out.println("Huawei: " + huawei.model + ", " + huawei.weight + " grams" + ", number: " + huawei.number);
+        huawei.receiveCall("Svetlana");
+        System.out.println(huawei.getNumber());
+
+
+    }
 }
